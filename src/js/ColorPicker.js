bbf378c2a23bd9353719f8071247a92bb0d455fd
Majo Chiou <majo.fee.chiou@gmail.com>
2016-04-25 00:23:53 +0800

    color

---
+++ src/js/ColorPicker.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/* *** 這部份用 ReactJs + redux 做 *** */
+
+import React from 'react';
+import RgbHex from 'rgb-hex';
+
+export default class ColorPicker extends React.Component {
+    constructor(props) {
+        super(props);
+        this.colorPick = this.colorPick.bind(this);
+    }
+
+    colorPick(e){
+        let _obj_self = e.target;
+        console.log( '----', _obj_self.style.backgroundColor );
+        this.props.whenClick( RgbHex(_obj_self.style.backgroundColor) );
+    }
+
+    render(){
+        let _json_style = {
+            color: 'transparent',
+            backgroundColor: this.props.color,
+            width: Math.floor(100/this.props.oneLine)+'%',
+            display: 'block',
+            float: 'left'
+        };
+        return (
+            <span style={_json_style} onClick={this.colorPick}>{this.props.color}</span>
+        );
+    }
+
+};
+
+ColorPicker.propTypes = {
+    color: React.PropTypes.string,
+    oneLine: React.PropTypes.number,
+    whenClick: React.PropTypes.func,
+},
+ColorPicker.defaultProps = {
+    color: '',
+    oneLine: 6,
+    whenClick: 6,
+};
--- src/js/MethodControlDot.js
+++ src/js/MethodControlDot.js
@@ -7,19 +7,37 @@ import GloablTools from './GloablTools';
 import Extend from 'Extend';
 import Settings from './Settings';
 import GloablData from './GloablData';
+import ColorPicker from './ColorPicker';
 
-export default class MethodControlAlpha extends React.Component {
+export default class MethodControlDot extends React.Component {
     constructor(props) {
         super(props);
 
+        this.setAllColor();
         this.arrangeProps( props );
 
         this.handleChangeRange = this.handleChangeRange.bind(this);
         this.submitAction = this.submitAction.bind(this);
+        this.colorPick = this.colorPick.bind(this);
     }
 
     getComponentMethod(){
         return Settings.METHOD_DOT;
+    }
+
+    arrangeState(){
+        return Extend.deep( 
+            {},
+            { control: this.props.control }, 
+            this.state, 
+            { control: {
+                frequency: this.refs.frequency.value,
+                minSize: this.refs.minSize.value,
+                maxSize: this.refs.maxSize.value,
+                minAlpha: this.refs.minAlpha.value,
+                maxAlpha: this.refs.maxAlpha.value
+            }
+        } );
     }
 
     componentWillReceiveProps(nextProps){
@@ -44,25 +62,60 @@ export default class MethodControlAlpha extends React.Component {
     }
 
     handleChangeRange() {
-        let _json_new = Extend.deep( 
-            {},
-            { control: this.props.control }, 
-            this.state, 
-            { control: {
-                frequency: this.refs.frequency.value,
-                minSize: this.refs.minSize.value,
-                maxSize: this.refs.maxSize.value,
-                minAlpha: this.refs.minAlpha.value,
-                maxAlpha: this.refs.maxAlpha.value
-            }
-        } );
+        let _json_new = this.arrangeState({});
         this.setState( _json_new );
+    }
+
+    setAllColor(){
+        let _scope = this;
+        let _ary_hex = ['0', '3', '6', '9', 'c', 'f'];
+        this.all_color = [];
+
+        for( let r=0; r<_ary_hex.length; r++ ){
+            for( let g=0; g<_ary_hex.length; g++ ){
+                for( let b=0; b<_ary_hex.length; b++ ){
+                    this.all_color.push('#'+_ary_hex[r]+_ary_hex[g]+_ary_hex[b]) ;
+                }
+            }
+        }
+    }
+
+    getAllColor(){
+        return this.all_color ;
+    }
+
+    colorPick( str_bg ){
+        let _scope = this ;
+        let _json_state = this.arrangeState();
+        _json_state.control.color = str_bg;
+        console.log('_json_state :: ', _json_state);
+        this.setState( _json_state );
+        setTimeout(function(){
+            console.log('_scope.state :: ', _scope.state);
+        },200);
     }
 
     render(){
         let _scope = this;
+        let _ary_hex_all = this.getAllColor();
+        let _num_oneline = 6;
+        let _json_style = {
+            display: 'block',
+            clear: 'both',
+            height: 0,
+            overflow: 'hidden',
+            position: 'relative'
+        };
+
         return (
             <div>
+                <div>{this.state.control.color}</div>
+                <div>
+                    <For each="str_item" of={ _ary_hex_all }>
+                        <ColorPicker color={str_item} oneLine={_num_oneline} whenClick={this.colorPick} />
+                    </For>
+                    <div style={_json_style}></div>
+                </div>
                 <div>
                     頻率 ： 
                     <input
@@ -119,9 +172,9 @@ export default class MethodControlAlpha extends React.Component {
 
 };
 
-MethodControlAlpha.propTypes = {
+MethodControlDot.propTypes = {
     control: React.PropTypes.object,
 },
-MethodControlAlpha.defaultProps = {
+MethodControlDot.defaultProps = {
     control: {},
 };
