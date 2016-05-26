4a8a11001cbb7ffe5fea9cf78933e0d3d054c5ab
Majo Chiou <majo.fee.chiou@gmail.com>
2016-05-26 11:26:17 +0800

    save

--- src/js/settingSection.js
+++ src/js/settingSection.js
@@ -14,6 +14,7 @@ import Extend from 'Extend';
 import Settings from './Settings';
 import StyleSettings from './StyleSettings';
 import ReactGroup from 'ReactGroup';
+import ReactGroupSetting from './../../lib/react-group/js/Setting';
 
 const OBJ_SETTING_STYLE = document.getElementById("setting-style");
 
@@ -28,11 +29,13 @@ export default class SettingSection extends GlobalConst {
     }
 
     getSelectKey(){
-        return 'value';
+        return ['value'];
     }
 
     getInputoption(){
-        return StyleSettings.getAllStyle();
+        let _sary = StyleSettings.getAllStyle();
+        console.log( '_sary :: ', _sary );
+        return _sary;
     }
 
     getOutputResult(){
@@ -40,7 +43,7 @@ export default class SettingSection extends GlobalConst {
     }
 
     getShowKey(){
-        return 'name';
+        return ['name'];
     }
 
     default(){
@@ -77,7 +80,7 @@ export default class SettingSection extends GlobalConst {
                 outputResult={this.getOutputResult()}
                 showKey={this.getShowKey()}
                 between="~"
-                display={_json_style.display}
+                display={ReactGroupSetting.DISPLAY_BLOCK}
                 padding={_json_style.padding}
                 fillet={_json_style.fillet}
                 listStyle={_json_style.listStyle}
@@ -85,7 +88,7 @@ export default class SettingSection extends GlobalConst {
                 iconPosition={_json_style.iconPosition}
                 iconShow={_json_style.iconShow}
                 styleName={_json_style.styleName}
-                composition={_json_style.composition}
+                composition={ReactGroupSetting.COMPOSITION_WHOLE}
                 offBack={_json_style.offBack}
                 styleBorder={_json_style.styleBorder}
                 styleIcon={_json_style.styleIcon}
